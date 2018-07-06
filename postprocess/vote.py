39fc25f308cd9bd3a8766f40765694a04e76201e
jovahe <qiaozh@mail.ustc.edu.cn>
2018-07-06 23:18:30 +0800

    reconstruct 20180706 Categorize all code files into 6 folders

---
+++ postprocess/vote.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+from tqdm import tqdm
+
+from ulitities.base_functions import load_img
+
+input_path = '../../data/predict/'
+input_masks=['unet/unet_combined_3.png','segnet/segnet_combined_3.png']
+output_file = '../../data/predict/final_result_3.png'
+
+
+def check_input_file(path, masks):
+    ret, img_1 = load_img(path+masks[0],grayscale=True)
+    assert (ret == 0)
+
+    height, width = img_1.shape
+    num_img = len(masks)
+
+    for next_index in range(1,num_img):
+        next_ret, next_img=load_img(path+masks[next_index],grayscale=True)
+        assert (next_ret ==0 )
+        next_height, next_width = next_img.shape
+        assert(height==next_height and width==next_width)
+    return height, width
+
+# each mask has 5 classes: 0~4
+
+def vote_per_image(height, width, path, masks):
+
+    mask_list = []
+    for tt in range(len(masks)):
+        ret, img = load_img(path+masks[tt],grayscale=True)
+        assert(ret ==0)
+        mask_list.append(img)
+
+    vote_mask=np.zeros((height,width), np.uint8)
+
+    for i in tqdm(range(height)):
+        for j in range(width):
+            record=np.zeros(256,np.uint8)
+            for n in range(len(mask_list)):
+                mask=mask_list[n]
+                pixel=mask[i,j]
+                record[pixel] +=1
+
+            """Alarming"""
+            # if record.argmax()==0: # if argmax of 0 = 125 or 255, not prior considering background(0)
+            #     record[0] -=1
+
+            label=record.argmax()
+            # print ("{},{} label={}".format(i,j,label))
+            vote_mask[i,j]=label
+
+    return vote_mask
+
+
+
+if __name__=='__main__':
+    x,y = check_input_file(input_path, input_masks)
+
+    final_mask = vote_per_image(x,y,input_path, input_masks)
+
+    cv2.imwrite(output_file, final_mask)
---
+++ predict/main.py
@@ -0,0 +1,160 @@
+#coding:utf8
+""""
+    This is main procedure for remote sensing image semantic segmentation
+
+"""
+import cv2
+import numpy as np
+import os
+import sys
+import argparse
+# from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+from sklearn.preprocessing import LabelEncoder
+from PIL import Image
+from keras.preprocessing.image import img_to_array
+#
+from segnet_predict import predict, predict_for_segnet_multiclassbands,get_predicted_pathces_from_image, mosaic_resut,predict_for_segnet_grayresult
+from smooth_tiled_predictions import predict_img_with_smooth_windowing_multiclassbands,cheap_tiling_prediction_not_square_img_multiclassbands
+
+from unet_predict import unet_predict,predict_for_unet_multiclassbands
+
+
+from keras import backend as K
+K.set_image_dim_ordering('th')
+# K.set_image_dim_ordering('tf')
+"""
+   The following global variables should be put into meta data file 
+"""
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+# segnet_classes = [0., 1., 2., 3., 4.]
+segnet_classes = [0., 1., 2.]
+segnet_dict = {'road':1, 'building':2}
+
+unet_classes = [0., 1.]
+
+
+FLAG_USING_UNET = False
+APPROACH_FLAG=4 # 0: raw predict; 1:flame tracer for smooth; 2: cheap predict; else:smooth predict
+
+
+input_image = '../../data/test/11.png'
+
+
+"""(1.1) for unet road predict"""
+# unet_model_path = '../../data/models/unet_channel_first_roads.h5'
+# unet_output_mask = '../../data/predict/unet/mask_unet_roads_'+os.path.split(test_image_path)[1]
+
+"""(1.2)for unet road predict"""
+unet_model_path = '../../data/models/unet_channel_first_buildings.h5'
+unet_output_mask = '../../data/predict/unet/mask_unet_buildings_'+os.path.split(input_image)[1]
+
+"""(2) for unet road predict"""
+segnet_model_path = '../../data/models/segnet_channel_first_012labels.h5' # for channel_first
+segnet_output_path = '../../data/predict/segnet/mask_segnet_'
+
+window_size = 256
+
+step = 128
+
+
+if __name__ == '__main__':
+
+    print("[INFO] opening image...")
+    if not os.path.isfile(input_image):
+        print("no file: {}".forma(input_image))
+        sys.exit(-1)
+
+    input_img = cv2.imread(input_image)
+    input_img = np.array(input_img, dtype="float") / 255.0  # must do it
+
+    print("[INFO] loading network...")
+    if FLAG_USING_UNET:
+        model = load_model(unet_model_path)
+        result_channels = len(unet_classes) - 1
+
+        labelencoder = LabelEncoder()
+        labelencoder.fit(unet_classes)
+
+    else:
+        model = load_model(segnet_model_path)
+
+        result_channels = len(segnet_classes) - 1
+
+        labelencoder = LabelEncoder()
+        labelencoder.fit(segnet_classes)
+
+    if APPROACH_FLAG ==0:
+        """0. test original code of predict()"""
+        if FLAG_USING_UNET:
+            result_test = unet_predict(input_img, model, window_size, labelencoder)
+        else:
+            result_test = predict(input_img, model, window_size, labelencoder)
+        cv2.imwrite('../data/predict/test.png', result_test)
+
+    elif APPROACH_FLAG==1:
+        """1. test code of flame tracer """
+        predicted_patches = get_predicted_pathces_from_image(
+            input_img,
+            model,
+            window_size,
+            step,
+            pre_func=predict_for_segnet_grayresult,
+            labelencoder=labelencoder)
+        mosaic_resut(predicted_patches)
+
+    elif APPROACH_FLAG==2:
+        """2. test cheap  predict"""
+        if FLAG_USING_UNET:
+            predictions_cheap = cheap_tiling_prediction_not_square_img_multiclassbands(
+                input_img,
+                model,
+                window_size=window_size,
+                real_classes=result_channels, # output channels = 是真的类别，总类别-背景
+                pred_func=predict_for_unet_multiclassbands,
+                labelencoder=labelencoder
+            )
+            cv2.imwrite(unet_output_mask, predictions_cheap)
+        else:
+            predictions_cheap = cheap_tiling_prediction_not_square_img_multiclassbands(
+                input_img,
+                model,
+                window_size=window_size,
+                real_classes=result_channels,  # output channels = 是真的类别，总类别-背景
+                pred_func=predict_for_segnet_multiclassbands,
+                labelencoder=labelencoder
+            )
+            for key,val in segnet_dict.items():
+                output_file = segnet_output_path+key+'.png'
+                cv2.imwrite(output_file, predictions_cheap[:,:,val-1])  # achieve the integer automatically
+
+    else:
+        """3. smooth predict """
+        if FLAG_USING_UNET:
+            predictions_smooth = predict_img_with_smooth_windowing_multiclassbands(
+                input_img,
+                model,
+                window_size=window_size,
+                subdivisions=2,
+                real_classes=result_channels,  # output channels = 是真的类别，总类别-背景
+                pred_func=predict_for_unet_multiclassbands,
+                labelencoder=labelencoder
+            )
+            cv2.imwrite(unet_output_mask, predictions_smooth)
+        else:
+            predictions_smooth = predict_img_with_smooth_windowing_multiclassbands(
+                input_img,
+                model,
+                window_size=window_size,
+                subdivisions=2,
+                real_classes=result_channels, # output channels = 是真的类别，总类别-背景
+                pred_func=predict_for_segnet_multiclassbands,
+                labelencoder=labelencoder
+            )
+            for key,val in segnet_dict.items():
+                output_file = segnet_output_path+key+'.png'  # try join([])
+                cv2.imwrite(output_file, predictions_smooth[:,:,val-1])
+                # imwrite () could achieve the int format  automatically
+
+

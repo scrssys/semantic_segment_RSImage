e0a6c10a48b2580177ac9701cf65044d7348a49c
jovahe <qiaozh@mail.ustc.edu.cn>
2019-03-28 19:22:24 +0800

    2019 version 0.0.2

---
+++ model_build/train_main.py
@@ -0,0 +1,323 @@
+# coding=utf-8
+import matplotlib
+
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.models import Sequential,load_model
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, Reshape, Permute, Activation, Input
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.image import img_to_array
+from keras.callbacks import ModelCheckpoint, EarlyStopping, History,ReduceLROnPlateau
+from keras.models import Model
+from keras.layers.merge import concatenate
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
+import random
+import sys
+import os
+import time
+from tqdm import tqdm
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+
+from keras import backend as K
+K.set_image_dim_ordering('tf')
+from keras.callbacks import TensorBoard
+
+
+# from semantic_segmentation_networks import binary_unet, binary_fcnnet, binary_segnet, binary_unet_onlyjaccard, binary_unet_jaccard
+from ulitities.base_functions import load_img_normalization,  load_img_by_gdal, UINT16, UINT8, UINT10
+
+
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+seed = 6
+np.random.seed(seed)
+from keras import metrics, losses
+from segmentation_models.losses import bce_jaccard_loss
+from segmentation_models.metrics import iou_score
+
+from segmentation_models import Unet,FPN,PSPNet,Linknet
+
+from utils import save, update_config
+from config import Config
+import json
+
+'''
+import  argparse
+parser=argparse.ArgumentParser(description='RS classification train')
+parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
+                        default=0, type=int)
+parser.add_argument('--config', dest='config_file', help='json file to config',
+                         default='config.json')
+args=parser.parse_args()
+gpu_id=args.gpu_id
+print("gpu_id:{}".format(gpu_id))
+os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
+
+config_file = args.config_file
+print("cofig file:{}".format(config_file))
+with open(args.config_file, 'r') as f:
+    cfg = json.load(f)
+'''
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+with open("config.json", 'r') as f:
+    cfg = json.load(f)
+config = Config(**cfg)
+print(config)
+import sys
+# sys.exit(-1)
+
+FLAG_MAKE_TEST=True
+im_type=UINT8
+if '10' in config.im_type:
+    im_type=UINT10
+elif "16" in config.im_type:
+    im_type=UINT16
+else:
+    pass
+
+date_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+print("date and time: {}".format(date_time))
+print("traindata from: {}".format(config.train_data_path))
+model_save_path = ''.join([config.model_dir,'/',config.network, '_',config.target_name, '_',config.BACKBONE,
+                           '_',config.loss,'_', date_time, '.h5'])
+print("model save as to: {}".format(model_save_path))
+
+"""get the train file name and divide to train and val parts"""
+def get_train_val(val_rate=config.val_rate):
+    train_url = []
+    train_set = []
+    val_set = []
+    for pic in os.listdir(config.train_data_path + 'src'):
+        train_url.append(pic)
+    random.shuffle(train_url)
+    total_num = len(train_url)
+    val_num = int(val_rate * total_num)
+    for i in range(len(train_url)):
+        if i < val_num:
+            val_set.append(train_url[i])
+        else:
+            train_set.append(train_url[i])
+    return train_set, val_set
+
+
+# data for training
+def generateData(batch_size, data=[]):
+    # print 'generateData...'
+    while True:
+        train_data = []
+        train_label = []
+        batch = 0
+        for i in (range(len(data))):
+            url = data[i]
+            batch += 1
+            _, img = load_img_normalization(config.im_bands, (config.train_data_path + '/src/' + url), data_type=im_type)
+            # Adapt dim_ordering automatically
+            img = img_to_array(img)
+            train_data.append(img)
+            _, label = load_img_normalization(1, (config.train_data_path + '/label/' + url))
+            label = img_to_array(label)
+            train_label.append(label)
+            if batch % batch_size == 0:
+                # print 'get enough bacth!\n'
+                train_data = np.array(train_data)
+                train_label = np.array(train_label)
+                if config.nb_classes>2:
+                    train_label = to_categorical(train_label, num_classes=config.nb_classes)
+                yield (train_data, train_label)
+                train_data = []
+                train_label = []
+                batch = 0
+
+
+# data for validation
+def generateValidData(batch_size, data=[]):
+    # print 'generateValidData...'
+    while True:
+        valid_data = []
+        valid_label = []
+        batch = 0
+        for i in (range(len(data))):
+            url = data[i]
+            batch += 1
+            _, img = load_img_normalization(config.im_bands, (config.train_data_path + '/src/' + url), data_type=im_type)
+            # Adapt dim_ordering automatically
+            img = img_to_array(img)
+            valid_data.append(img)
+            _, label = load_img_normalization(1, (config.train_data_path + '/label/' + url))
+            label = img_to_array(label)
+            valid_label.append(label)
+            if batch % batch_size == 0:
+                valid_data = np.array(valid_data)
+                valid_label = np.array(valid_label)
+                if config.nb_classes>2:
+                    valid_label = to_categorical(valid_label, num_classes=config.nb_classes)
+                yield (valid_data, valid_label)
+                valid_data = []
+                valid_label = []
+                batch = 0
+
+"""Train model ............................................."""
+def train(model):
+
+    if os.path.isfile(config.base_model):
+        model.load_weights(config.base_model)
+        print("load last weight from:{}".format(config.base_model))
+
+    model_checkpoint = ModelCheckpoint(
+        model_save_path,
+        monitor=config.monitor,
+        save_best_only=config.save_best_only)
+
+    model_earlystop = EarlyStopping(
+        monitor=config.monitor,
+        patience=config.patience,
+        verbose=0,
+        mode=config.mode)
+
+    # """自动调整学习率"""
+    model_reduceLR=ReduceLROnPlateau(
+        monitor=config.monitor,
+        factor=config.factor,
+        patience=config.patience,
+        verbose=0,
+        mode=config.mode,
+        epsilon=config.epsilon,
+        cooldown=config.cooldown,
+        min_lr=config.min_lr
+    )
+
+    model_history = History()
+
+    logdir = ''.join([config.log_dir,'/log',config.target_name,"_", config.network,"_",config.BACKBONE,"_", config.loss, date_time])
+    if not os.path.isdir(logdir):
+        print("Warning: ")
+        os.mkdir(logdir)
+
+    tb_log = TensorBoard(log_dir=logdir)
+
+    callable = [model_checkpoint,model_earlystop, model_reduceLR, model_history, tb_log]
+    # callable = [model_checkpoint, model_reduceLR, model_history, tb_log]
+    # callable = [model_checkpoint]
+
+    train_set, val_set = get_train_val()
+    train_numb = len(train_set)
+    valid_numb = len(val_set)
+    print ("the number of train data is", train_numb)
+    print ("the number of val data is", valid_numb)
+
+    H = model.fit_generator(generator=generateData(config.batch_size,train_set),
+                            steps_per_epoch=train_numb // config.batch_size, epochs=config.epochs,
+                            verbose=1,
+                            validation_data=generateValidData(config.batch_size, val_set),
+                            validation_steps=valid_numb // config.batch_size,
+                            callbacks=callable,
+                            max_q_size=1)
+
+"""
+Test the model which has been trained right now
+"""
+window_size=config.img_w
+def test_predict(bands, image,model):
+    stride = window_size
+
+    h, w, _ = image.shape
+    print('h,w:', h, w)
+    padding_h = (h // stride + 1) * stride
+    padding_w = (w // stride + 1) * stride
+    padding_img = np.zeros((padding_h, padding_w, bands))
+    padding_img[0:h, 0:w, :] = image[:, :, :]
+
+    padding_img = img_to_array(padding_img)
+
+    mask_whole = np.zeros((padding_h, padding_w), dtype=np.float32)
+    for i in list(range(padding_h // stride)):
+        for j in list(range(padding_w // stride)):
+            crop = padding_img[i * stride:i * stride + window_size, j * stride:j * stride + window_size, :bands]
+
+            crop = np.expand_dims(crop, axis=0)
+            print('crop:{}'.format(crop.shape))
+
+            pred = model.predict(crop, verbose=2)
+            # pred = np.argmax(pred, axis=2)  #for one hot encoding
+            pred[pred < 0.5] = 0
+            pred[pred >= 0.5] = 1
+
+            pred = pred.reshape(256, 256)
+            print(np.unique(pred))
+
+            mask_whole[i * stride:i * stride + window_size, j * stride:j * stride + window_size] = pred[:, :]
+
+    outputresult =mask_whole[0:h,0:w]
+    # outputresult = outputresult.astype(np.uint8)
+
+    plt.imshow(outputresult, cmap='gray')
+    plt.title("Original predicted result")
+    plt.show()
+    cv2.imwrite('../../data/predict/test_model_new.png',outputresult*255)
+    return outputresult
+
+if __name__ == '__main__':
+
+    if not os.path.isdir(config.train_data_path):
+        print ("train data does not exist in the path:\n {}".format(config.train_data_path))
+
+
+    input_layer = (config.img_w, config.img_h, config.im_bands)
+    model = Unet(backbone_name=config.BACKBONE, input_shape=input_layer,
+                 classes=config.nb_classes, activation=config.activation,
+                 encoder_weights=config.encoder_weights)
+    if 'pspnet' in config.network:
+        model = PSPNet(backbone_name=config.BACKBONE, input_shape=input_layer,
+                     classes=config.nb_classes, activation=config.activation,
+                     encoder_weights=config.encoder_weights)
+    elif 'fpn' in config.network:
+        model = FPN(backbone_name=config.BACKBONE, input_shape=input_layer,
+                     classes=config.nb_classes, activation=config.activation,
+                     encoder_weights=config.encoder_weights)
+    elif 'linknet' in config.network:
+        model = Linknet(backbone_name=config.BACKBONE, input_shape=input_layer,
+                     classes=config.nb_classes, activation=config.activation,
+                     encoder_weights=config.encoder_weights)
+    else:
+        pass
+
+    self_optimizer = SGD(lr=config.lr, decay=1e-6, momentum=0.9, nesterov=True)
+    if 'adagrad' in config.optimizer:
+        self_optimizer = Adagrad(lr=config.lr, decay=1e-6)
+    elif 'adam' in config.optimizer:
+        self_optimizer = Adam(lr=config.lr, decay=1e-6)
+    else:
+        pass
+
+    model.compile(self_optimizer, loss=config.loss, metrics=[config.metrics])
+    # model.compile('SGD', loss=bce_jaccard_loss, metrics=[iou_score])
+
+    print(model.summary())
+    print("Train by : {}_{}".format(config.network, config.BACKBONE))
+    train(model)
+
+    if FLAG_MAKE_TEST:
+        print("test ....................predict by trained model .....\n")
+        test_img_path = '../../data/test/sample1.png'
+        import sys
+
+        if not os.path.isfile(test_img_path):
+            print("no file: {}".format(test_img_path))
+            sys.exit(-1)
+
+        input_img = load_img_by_gdal(test_img_path)
+        if im_type == UINT8:
+            input_img = input_img / 255.0
+        elif im_type == UINT10:
+            input_img = input_img / 1024.0
+        elif im_type == UINT16:
+            input_img = input_img / 65535.0
+        input_img = np.clip(input_img, 0.0, 1.0)
+
+        new_model = load_model(model_save_path)
+
+        test_predict(input_img, new_model)
